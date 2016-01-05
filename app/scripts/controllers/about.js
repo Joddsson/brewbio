9b02f2b5979976971a863bde2b2a2b449dfa2f0a
Ívar <ivaroddsson@gmail.com>
2016-01-05 21:00:51 +0000

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name brewbioApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the brewbioApp
+ */
+angular.module('brewbioApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name brewbioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the brewbioApp
+ */
+angular.module('brewbioApp')
+  .controller('MainCtrl', function () {
+
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
